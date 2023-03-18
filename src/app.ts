ca919c9409dad0bc6f664a58e3be573dcae68d54
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-03-18 16:30:58 -0300

    Requisito 4

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import productRoute from './router/product.route';
 import userRoute from './router/user.route';
+import orderRoute from './router/order.route';
 
 const app = express();
 
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json());
 app.use('/products', productRoute);
 app.use('/users', userRoute);
+app.use('/orders', orderRoute);
 
 export default app;
